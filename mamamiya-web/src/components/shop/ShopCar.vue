62f7f298233c7f93489dbefd57a4274d2f8e322a
Fudada <1170559835@qq.com>
2021-01-24 22:52:18 +0800

    添加了购物车

--- mamamiya-web/src/components/shop/ShopCar.vue
+++ mamamiya-web/src/components/shop/ShopCar.vue
@@ -2,13 +2,23 @@
   <div id="shopcar" class="max-width margin-auto position-re">
       <div class="shopnav">
           <div class="shopnav-item">全部商品</div>
-          <div class="shopnav-item">库存紧张</div>
+          <!-- <div class="shopnav-item">库存紧张</div> -->
       </div>
       <div class="line max-widht margin-auto"></div>
       <div class="shopitems max-width margin-auto">
           <!-- 全部商品 -->
           <div id="allitem">
-                <div class="item" v-for="(item,index) in 10" :key="index"></div>
+                <div class="item" v-for="(item,index) in 19" :key="index">
+                    <input type="checkbox" class="iswant">
+                    <img src="https://img.alicdn.com/bao/uploaded/i1/2616970884/TB1gcGcpIj_B1NjSZFHXXaDWpXa_!!2-item_pic.png_80x80.jpg" alt="" class="itemimg">
+                    <p class="iteminfo">title</p>
+                    <div class="selectinfo"></div>
+                    <el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字" class="countnums"></el-input-number>
+                    <p class="needprice">¥500</p>
+                    <div class="buttons">
+                     <el-button type="danger">删除</el-button>
+                     </div>
+                </div>
           </div> 
           <!-- 紧张库存 -->
           <div id="queshaoitem">
@@ -26,7 +36,8 @@
           <div class="toright">
           <div><p class="items">已选3件宝贝</p></div>
           <div><p class="acount">合计(不含运费)：100元</p></div>
-          <div><p class="okok">结算</p></div>
+           <b-button variant="danger" style="margin-top:20px;width=100px">结算</b-button>
+           
           </div>
       </div>
   </div>
@@ -35,44 +46,63 @@
 <script>
 export default {
     methods: {
+        data() {
+            return {
+                num: 1
+            }
+        },
  
-        // handleScroll () {
-        //     var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-        //     // var  offsetTop = document.getElementById("dealplane").offsetTop;
-        //     let oo = document.getElementById("dealplane"); 
-        //     // console.log(offsetTop);
-            
-        //     if(scrollTop > 0){
-        //         oo.style.position = fixed;
-        //     }
-        // }
+        handleScroll () {
+            // var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+
+           let scrollTop = document.documentElement.scrollTop;
+           let scrollHeight = document.documentElement.scrollHeight;
+           let clientHeight = document.documentElement.clientHeight;
+      
+           let ele = document.getElementById("dealplane");
+           console.log(scrollHeight - scrollTop );
+        //    这 "=" 超级重要,因为没有等号的话，好到到达300，样式改变，导致要滑动的距离又增长，结果出现死循环而发生卡屏
+           if(scrollHeight - scrollTop <= clientHeight+300){
+                ele.style.position = "static";
+               
+           }else {
+               ele.style.position = "fixed";
+            //    ele.style.margin=0;
+           }
+
+        }
      
     },
     created() {
        
+    },
+    mounted(){
+      
+        window.addEventListener('scroll',this.handleScroll)
+        
     }
-    // mounted(){
-    //     window.addEventListener('scroll',this.handleScroll)
-    // }
 }
 
 
 </script>
 
 <style lang="scss" scoped>
+// 购物车最大盒子
 #shopcar{
     /* border: 1px solid gold; */
 }
+// 
 .shopnav{
     width: inherit;
 }
-/* 防止margin塌陷 */
+/* 防止margin塌陷 下面那条横线 */
 .line{
     height: 2px;
     border: 2px solid rgb(226, 226, 226);
     clear: both;
     margin-bottom: 15px;
 }
+// 全部商品 库存紧张
 .shopnav-item{
     float: left;
     width: 150px;
@@ -82,24 +112,72 @@ export default {
     line-height: 50px;
     margin-right: 10px;
 }
+// 每个物品的大框
 .shopitems{
     /* border: 1px solid salmon; */
     
 }
+// 每个物品
 .item{
     border: 1px solid rgb(226, 226, 226);
     height: 150px;
     margin-top: 10px;
  
 }
-.dealplane{
-    position: fixed;
+// 每个item栏里的样式
+.iswant{
+    float: left;
+    margin-top:25px;
+    margin-left: 35px;
+}
+.itemimg{
+    float: left;
+    width: 100px;
+    height: 100px;
+    margin-top: 25px;
+    margin-left: 20px;
+}
+.iteminfo{
+    float: left;
+    margin-top: 25px;
+    margin-left: 25px;
+    width: 250px;
+    border: 1px solid greenyellow;
+}
+.selectinfo{
+    float: left;
+    width: 200px;
+    margin-top: 25px;
+    height: 100px;
+    margin-left: 25px;
+    border: 1px solid goldenrod;
+}
+.countnums{
+    float: left;
+    margin-top: 25px;
+    margin-left: 20px;
+}
+.needprice{
+    float: left;
+    width: 50px;
+    margin-top: 25px;
+    margin-left: 10px;
+}
+.buttons{
+    margin: 25px;
+}
+// 这是下面结算栏的样式
+#dealplane{
+    // display: none;
+    // position: fixed;
+    margin-top: 80px;
     bottom: 0px;
     z-index: 1;
     height: 80px;
     border: 1px solid gainsboro;
     overflow: hidden;
     background-color: #fff;
+    // margin-bottom: 100px;
 }
 .allcheck{
     float:left;
@@ -109,12 +187,17 @@ export default {
     margin-left: 20px;
 }
 .dealplane{
+     .toright{
+         width: 450px;
+         float: right;
+     }
+}
+.dealplane{
     p{
         float: left;
         margin-top: 27px;
         margin-left: 15px;
     }
 }
-
 
 </style>
